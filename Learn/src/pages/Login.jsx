3afa0656a38e3ccd68505592bffe036a761d33b8
NEYAZUDDIN5050 <132392596+NEYAZUDDIN5050@users.noreply.github.com>
2024-11-25 12:02:13 +0530

    Login

--- Learn/src/pages/Login.jsx
+++ Learn/src/pages/Login.jsx
@@ -1,16 +1,53 @@
 import React from "react";
 
-function Login({ signIn }) {
+const Login = () => {
   return (
-    <div className="w-full h-screen flex content-center justify-center items-center bg-blue-950">
-      <button
-        onClick={signIn}
-        className="px-5 py-2 bg-gray-900 outlinr-none text-white font-bold rounded"
-      >
-        LOG ME IN
-      </button>
+    <div className="relative flex flex-col justify-center min-h-screen overflow-hidden   ">
+      <div className="w-full p-6 m-auto bg-blue rounded-md shadow-md lg:max-w-xl">
+        <h1 className="text-3xl font-semibold text-center text-purple-700 underline">
+          Login
+        </h1>
+        <form>
+          <div>
+            <label className="block text-sm font-semibold text-grey-800">
+              Email
+            </label>
+            <input
+              type="email"
+              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div>
+            <label
+              for="passward"
+              className="block text-sm font-semibold text-grey-800"
+            >
+              Passward
+            </label>
+            <input
+              type="Passward"
+              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+            <a href="#" className="text-xs text-purple-600 hover:underline">
+              Forget Password?
+            </a>
+            <div className="mt-6">
+              <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+                LOG in me
+              </button>
+            </div>
+          </div>
+        </form>
+        <p className="mt-8 text-xs font-light text-center text-gray-700">
+          {" "}
+          Don't have an account?{" "}
+          <a href="#" className="font-medium text-purple-600 hover:underline">
+            Sign up
+          </a>
+        </p>
+      </div>
     </div>
   );
-}
+};
 
 export default Login;
